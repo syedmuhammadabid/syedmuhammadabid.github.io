3aa3faad62b2f3e1ae1c0cde162d99184ae3a519
Abid Hussain <19214455+syedmuhammadabid@users.noreply.github.com>
2026-07-07 19:29:20 +0500

    Contact details added

--- src/components/githubProfileCard/GithubProfileCard.js
+++ src/components/githubProfileCard/GithubProfileCard.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./GithubProfileCard.scss";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
-import {contactInfo, isHireable} from "../../portfolio";
+import { contactInfo, isHireable } from "../../portfolio";
 import emoji from "react-easy-emoji";
-import {Fade} from "react-reveal";
+import { Fade } from "react-reveal";
 
-export default function GithubProfileCard({prof}) {
+export default function GithubProfileCard({ prof }) {
   if (isHireable) {
     prof.hireable = "Yes";
   } else {
@@ -44,6 +44,16 @@ export default function GithubProfileCard({prof}) {
             <div className="opp-div">
               <span className="desc-prof">
                 Open for opportunities: {prof.hireable}
+              </span>
+            </div>
+            <div>
+              <span className="contact-details">
+                Email: syedmuhammadabid110@gmail.com
+              </span>
+            </div>
+            <div className="opp-div">
+              <span className="contact-details">
+                Phone: +92 321 286 9005
               </span>
             </div>
             <SocialMedia />
