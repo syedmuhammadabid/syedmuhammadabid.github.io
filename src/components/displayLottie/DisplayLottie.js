2fcf2ca8be0776562129f06ec6c184fc07ca7084
Abid Hussain <19214455+syedmuhammadabid@users.noreply.github.com>
2026-07-08 15:23:09 +0500

    Performance optimization for the portfolio

--- src/components/displayLottie/DisplayLottie.js
+++ src/components/displayLottie/DisplayLottie.js
@@ -1,23 +1,12 @@
-import React, {Component, Suspense} from "react";
-import Lottie from "lottie-react";
+import React, {Suspense, lazy} from "react";
 import Loading from "../../containers/loading/Loading";
 
-export default class DisplayLottie extends Component {
-  render() {
-    const animationData = this.props.animationData;
-    const defaultOptions = {
-      loop: true,
-      autoplay: true,
-      animationData: animationData
-    };
+const Lottie = lazy(() => import("lottie-react"));
 
-    return (
-      <Suspense fallback={<Loading />}>
-        <Lottie
-          animationData={defaultOptions.animationData}
-          loop={defaultOptions.loop}
-        />
-      </Suspense>
-    );
-  }
+export default function DisplayLottie({animationData}) {
+  return (
+    <Suspense fallback={<Loading />}>
+      <Lottie animationData={animationData} loop={true} />
+    </Suspense>
+  );
 }
